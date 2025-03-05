2df94bf9f0f243b1fb97d9b78181bb0c78cdb179
igoryen <igor.yentaltsev@gmail.com>
2025-03-05 15:03:53 -0500

    added ss05

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home";
 import SS04 from "./pages/SS04";
+import SS05 from "./pages/SS05";
 import AB01 from "./pages/AB01";
 import Bob from "./pages/Bob";
 import Jaw from "./pages/Jaw";
@@ -20,6 +21,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="ab01" element={<AB01 />} />
           <Route path="ss04" element={<SS04 />} />
+          <Route path="ss05" element={<SS05 />} />
           <Route path="bob" element={<Bob />} />
           <Route path="jaw" element={<Jaw />} />
           <Route path="leo" element={<Leo />} />
