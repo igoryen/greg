fffb123addc79c203b1c36c6ac99effe41bb3a95
igoryen <igor.yentaltsev@gmail.com>
2025-03-09 19:47:11 -0400

    added ip and ip03

---
+++ src/pages/IP.js
@@ -0,0 +1,18 @@
+import "../styles/fc.scss";
+import React from "react";
+
+class IP extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Anglofor</h3>
+                <h1>Ihor Pavelkiv</h1>
+                <section className="tom">
+                    <p><a href="/ip03" rel="noopener noreferrer">Lesson 3</a></p>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default IP;
