176ac623156ddaed7430114772b12d3380f65f7d
igoryen <igor.yentaltsev@gmail.com>
2025-03-11 01:15:52 -0400

    added route for ip04

--- src/pages/IP.js
+++ src/pages/IP.js
@@ -9,6 +9,7 @@ class IP extends React.Component {
                 <h1>Ihor Pavelkiv</h1>
                 <section className="tom">
                     <p><a href="/ip03" rel="noopener noreferrer">Lesson 3</a></p>
+                    <p><a href="/ip04" rel="noopener noreferrer">Lesson 4</a></p>
                 </section>
             </div>
         );
