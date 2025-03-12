3e5a11fdb23c24113840b8f5db941cf84ab21510
igoryen <igor.yentaltsev@gmail.com>
2025-03-12 15:09:58 -0400

    Leon: tiny correction

--- src/components/Leon.js
+++ src/components/Leon.js
@@ -2,7 +2,7 @@ function Leon({ lesson }) {
     return (
         <section>
             <h3>Anglofor</h3>
-            <h1><a href={`/${lesson.slug}`} rel="noopener noreferrer" > {lesson.student} </a > :: Lesson {lesson.number}</h1>
+            <h1><a href={`/${lesson.slug}`} rel="noopener noreferrer" >{lesson.student}</a > :: Lesson {lesson.number}</h1>
         </section>
     );
 }
