fdf931ad9556e6b90a1af0255cbd6bfa41a41b07
igoryen <igor.yentaltsev@gmail.com>
2025-03-11 20:29:44 -0400

    ab05: added more FC

--- src/pages/AB05.js
+++ src/pages/AB05.js
@@ -340,9 +340,27 @@ class AB05 extends React.Component {
             },
             {
                 "cefr": "",
+                "def1": "скажи нам",
+                "ipa": "tɛl ʌs",
+                "spell": "tell us"
+            },
+            {
+                "cefr": "",
                 "def1": "покупка",
                 "ipa": "ˈpɜrʧəs",
                 "spell": "purchase"
+            },
+            {
+                "cefr": "",
+                "def1": "на работе",
+                "ipa": "æt wɜrk",
+                "spell": "at work"
+            },
+            {
+                "cefr": "",
+                "def1": "на работу",
+                "ipa": "tu wɜrk",
+                "spell": "to work"
             },
             {
                 "cefr": "",
@@ -352,9 +370,27 @@ class AB05 extends React.Component {
             },
             {
                 "cefr": "",
+                "def1": "иди сюда",
+                "ipa": "kʌm hir",
+                "spell": "come here"
+            },
+            {
+                "cefr": "",
+                "def1": "уйди!",
+                "ipa": "ɡɛt əˈweɪ!",
+                "spell": "get away!"
+            },
+            {
+                "cefr": "",
                 "def1": "сильнее",
                 "ipa": "ˈstrɔŋɡər",
                 "spell": "stronger"
+            },
+            {
+                "cefr": "",
+                "def1": "вижу её",
+                "ipa": "aɪ si hɜr",
+                "spell": "I see her"
             },
             {
                 "cefr": "",
@@ -376,15 +412,183 @@ class AB05 extends React.Component {
             },
             {
                 "cefr": "",
+                "def1": "слышу его",
+                "ipa": "aɪ hir ɪt",
+                "spell": "I hear it"
+            },
+            {
+                "cefr": "",
+                "def1": "я тебя вижу",
+                "ipa": "aɪ si ju",
+                "spell": "I see you"
+            },
+            {
+                "cefr": "",
                 "def1": "буду, будем, будешь...",
                 "ipa": "wɪl",
                 "spell": "will"
             },
             {
                 "cefr": "",
+                "def1": "слышу это",
+                "ipa": "aɪ hir ðæt",
+                "spell": "I hear that"
+            },
+            {
+                "cefr": "",
+                "def1": "могу говорить",
+                "ipa": "aɪ kæn tɔk",
+                "spell": "I can talk"
+            },
+            {
+                "cefr": "",
+                "def1": "я вас услышала",
+                "ipa": "aɪ hir ju",
+                "spell": "I hear you"
+            },
+            {
+                "cefr": "",
+                "def1": "я спрашиваю тебя",
+                "ipa": "aɪ æsk ju",
+                "spell": "I ask you"
+            },
+            {
+                "cefr": "",
+                "def1": "следуй(те) за мной",
+                "ipa": "ˈfɑloʊ mi",
+                "spell": "follow me"
+            },
+            {
+                "cefr": "",
+                "def1": "(по)говори со мной",
+                "ipa": "tɔk tu mi",
+                "spell": "talk to me"
+            },
+            {
+                "cefr": "",
+                "def1": "иди(те) со мной",
+                "ipa": "kʌm wɪð mi",
+                "spell": "come with me"
+            },
+            {
+                "cefr": "",
+                "def1": "дай мне деньги",
+                "ipa": "ɡɪv mi ˈmʌni",
+                "spell": "give me money"
+            },
+            {
+                "cefr": "",
                 "def1": "дать ей звездюлей",
                 "ipa": "kɪk hɜr bʌt",
                 "spell": "kick her butt"
+            },
+            {
+                "cefr": "",
+                "def1": "даю ей деньги",
+                "ipa": "aɪ ɡɪv hɜr ˈmʌni",
+                "spell": "I give her money"
+            },
+            {
+                "cefr": "",
+                "def1": "видишь ли ты меня?",
+                "ipa": "kæn ju si mi?",
+                "spell": "can you see me?"
+            },
+            {
+                "cefr": "",
+                "def1": "полиция видит нас",
+                "ipa": "ðə pəˈlis si ʌs",
+                "spell": "the police see us"
+            },
+            {
+                "cefr": "",
+                "def1": "это не для меня",
+                "ipa": "ðɪs ɪz nɑt fɔr mi",
+                "spell": "this is not for me"
+            },
+            {
+                "cefr": "",
+                "def1": "даю деньги ей",
+                "ipa": "aɪ ɡɪv ˈmʌni tu hɜr",
+                "spell": "I give money to her"
+            },
+            {
+                "cefr": "",
+                "def1": "полиция видит их",
+                "ipa": "ðə pəˈlis si ðɛm",
+                "spell": "the police see them"
+            },
+            {
+                "cefr": "",
+                "def1": "иди прочь от меня!",
+                "ipa": "ɡoʊ əˈweɪ frʌm mi!",
+                "spell": "go away from me!"
+            },
+            {
+                "cefr": "",
+                "def1": "я последую за тобой",
+                "ipa": "aɪ wɪl ˈfɑloʊ ju",
+                "spell": "I will follow you"
+            },
+            {
+                "cefr": "",
+                "def1": "нас не догонят",
+                "ipa": "ðeɪ wɪl nɑt kæʧ ʌs",
+                "spell": "they will not catch us"
+            },
+            {
+                "cefr": "",
+                "def1": "я услышала сестру",
+                "ipa": "aɪ hɜrd maɪ ˈsɪstər",
+                "spell": "I heard  my sister"
+            },
+            {
+                "cefr": "",
+                "def1": "ему дали зарплату",
+                "ipa": "ðeɪ ɡeɪv hɪm ə ˈsæləri",
+                "spell": "they gave him a salary"
+            },
+            {
+                "cefr": "",
+                "def1": "работа была сделана нами",
+                "ipa": "ðə wɜrk wʌz dʌn baɪ ʌs",
+                "spell": "the work was done by us"
+            },
+            {
+                "cefr": "",
+                "def1": "дайте нам денег",
+                "ipa": "ɡɪv ʌs ˈmʌni, ɡɪv ˈmʌni tu ʌs",
+                "spell": "give us money, give money to us"
+            },
+            {
+                "cefr": "",
+                "def1": "эта работа была сделана им",
+                "ipa": "ðɪs wɜrk wʌz dʌn baɪ hɪm",
+                "spell": "this work was done by him"
+            },
+            {
+                "cefr": "",
+                "def1": "не расскажу вам ничего",
+                "ipa": "aɪ wɪl nɑt tɛl ju ˈɛniˌθɪŋ",
+                "spell": "I will not tell you anything"
+            },
+            {
+                "cefr": "",
+                "def1": "расскажи мне про покупки",
+                "ipa": "tɛl mi əˈbaʊt jʊər ˈpɜrʧəsəz",
+                "spell": "tell me about your purchases"
+            },
+            {
+                "cefr": "",
+                "def1": "зарплата данная ему была маленькой",
+                "ipa": "ðə ˈsæləri ˈɡɪvən tu hɪm wʌz smɔl",
+                "spell": "the salary given to him was small"
+            },
+            {
+                "cefr": "",
+                "def1": "я даю тебе деньги, а ты работаешь на меня",
+                "ipa": "aɪ ɡɪv ju ˈmʌni, ænd ju wɜrk fɔr mi",
+                "spell": "I give you money, and you work for me"
             }
         ];
         const wordsFinal = [];
