aac07e2e52bbf8a337c0ecf930d09aeecc716a8a
igoryen <igor.yentaltsev@gmail.com>
2025-03-26 21:58:50 -0400

    Word: added shuffle function

--- src/components/Word.js
+++ src/components/Word.js
@@ -6,6 +6,25 @@ function Word({ words }) {
         window.$(".word").flip({ speed: 200 });
     });
 
+    // src: https://stackoverflow.com/a/2450976/2938344
+    function shuffle(array) {
+        let currentIndex = array.length;
+
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+
+            // Pick a remaining element...
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+    }
+
+    shuffle(words);
+
     const wordsFinal = [];
 
     for (let [i, word] of words.entries()) {
@@ -24,6 +43,7 @@ function Word({ words }) {
             </div>
         );
     }
+
     return (
         <section className="words">
             {wordsFinal}
