b1452b260236c81ce6bc4762eadca8116a2b2d65
igoryen <igor.yentaltsev@gmail.com>
2025-03-30 09:03:57 -0400

    ab: added lesson dates

--- src/pages/AB.js
+++ src/pages/AB.js
@@ -8,15 +8,15 @@ class AB extends React.Component {
                 <h3>Anglofor</h3>
                 <h1>Alena</h1>
                 <section className="tom">
-                    <p><a href="/ab12" rel="noopener noreferrer">Lesson 12</a></p>
-                    <p><a href="/ab10" rel="noopener noreferrer">Lesson 10</a></p>
-                    <p><a href="/ab09" rel="noopener noreferrer">Lesson 9</a></p>
-                    <p><a href="/ab08" rel="noopener noreferrer">Lesson 8</a></p>
-                    <p><a href="/ab07" rel="noopener noreferrer">Lesson 7</a></p>
-                    <p><a href="/ab06" rel="noopener noreferrer">Lesson 6</a></p>
-                    <p><a href="/ab05" rel="noopener noreferrer">Lesson 5</a></p>
-                    <p><a href="/ab04" rel="noopener noreferrer">Lesson 4</a></p>
-                    <p><a href="/ab01" rel="noopener noreferrer">Lesson 1</a></p>
+                    <p><a href="/ab12" rel="noopener noreferrer">Lesson 12</a><span>2025-03-27 (Thu)</span></p>
+                    <p><a href="/ab10" rel="noopener noreferrer">Lesson 10</a><span>2025-03-22 (Sat)</span></p>
+                    <p><a href="/ab09" rel="noopener noreferrer">Lesson 9</a><span>2025-03-20 (Thu)</span></p>
+                    <p><a href="/ab08" rel="noopener noreferrer">Lesson 8</a><span>2025-03-18 (Tue)</span></p>
+                    <p><a href="/ab07" rel="noopener noreferrer">Lesson 7</a><span>2025-03-16 (Sun)</span></p>
+                    <p><a href="/ab06" rel="noopener noreferrer">Lesson 6</a><span>2025-03-13 (Thu)</span></p>
+                    <p><a href="/ab05" rel="noopener noreferrer">Lesson 5</a><span>2025-03-11 (Tue)</span></p>
+                    <p><a href="/ab04" rel="noopener noreferrer">Lesson 4</a><span>2025-03-09 (Sun)</span></p>
+                    <p><a href="/ab01" rel="noopener noreferrer">Lesson 1</a><span>2025-03-02 (Sun)</span></p>
                 </section>
             </div>
         );
