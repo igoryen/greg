29ed22eeaddd12ff4304217895c02245c0b924cf
igoryen <igor.yentaltsev@gmail.com>
2025-04-17 08:05:38 -0400

    ml40: added

--- src/index.js
+++ src/index.js
@@ -82,6 +82,7 @@ import ML34 from "./pages/ml/ML34";
 import ML35 from "./pages/ml/ML35";
 import ML37 from "./pages/ml/ML37";
 import ML38 from "./pages/ml/ML38";
+import ML40 from "./pages/ml/ML40";
 
 import MS from "./pages/ms/MS";
 import MS06 from "./pages/ms/MS06";
@@ -210,6 +211,7 @@ export default function App() {
           <Route path="ml35" element={<ML35 />} />
           <Route path="ml37" element={<ML37 />} />
           <Route path="ml38" element={<ML38 />} />
+          <Route path="ml40" element={<ML40 />} />
 
           <Route path="ms" element={<MS />} />
           <Route path="ms06" element={<MS06 />} />
---
+++ src/pages/ml/ML40.js
@@ -0,0 +1,25 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import Mathilda from "../../components/Mathilda";
+import words from "../../data/ml/ml40.json";
+
+class ML40 extends React.Component {
+
+    render() {
+        const lesson = {
+            "student": "Maria",
+            "number": 40,
+            "slug": "ml"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <Mathilda words={words} />
+            </div>
+        );
+    }
+}
+
+export default ML40;
