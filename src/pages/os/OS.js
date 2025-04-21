c748831a2975d30a026a2b3eddd85f071a4e6b5f
igoryen <igor.yentaltsev@gmail.com>
2025-04-21 18:28:27 -0400

    Derek: added to OS

--- src/pages/os/OS.js
+++ src/pages/os/OS.js
@@ -1,16 +1,15 @@
 import "../../styles/fc.scss";
 import React from "react";
+import Derek from "../../components/Derek";
+import lines from "../../data/os/os.json";
 
 class OS extends React.Component {
     render() {
         return (
             <div>
                 <h3>Anglofor</h3>
-                <h1>Alex S.</h1>
-                <section className="tom">
-                    <p><a href="/os02" rel="noopener noreferrer">Lesson 2</a><span>2025-03-28 (Fri)</span></p>
-                    <p><a href="/os01" rel="noopener noreferrer">Lesson 1</a><span>2025-03-24 (Mon)</span></p>
-                </section>
+                <h1>Alex</h1>
+                <Derek derek={lines} slug="os" />
             </div>
         );
     }
