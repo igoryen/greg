15e8e9f79cfd8e979ebc58436c6e8c4695809b92
igoryen <igor.yentaltsev@gmail.com>
2025-04-30 17:34:14 -0400

    ip26: added

---
+++ src/pages/ip/IP26.js
@@ -0,0 +1,25 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import words from "../../data/ip/ip26.json";
+import Mathilda from "../../components/Mathilda";
+
+class IP26 extends React.Component {
+
+    render() {
+        const lesson = {
+            "student": "Ihor",
+            "number": 26,
+            "slug": "ip"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <Mathilda words={words} />
+            </div>
+        );
+    }
+}
+
+export default IP26;
