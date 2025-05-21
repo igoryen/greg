51765a3818bf511573a8ddf7a7ee48db77ef69c5
igoryen <igor.yentaltsev@gmail.com>
2025-05-20 23:58:56 -0400

    ab31: added

---
+++ src/pages/ab/AB31.js
@@ -0,0 +1,39 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import Mathilda from "../../components/Mathilda";
+import words from "../../data/ab/ab31.json";
+
+class AB31 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: "def1"
+        }
+    }
+
+    setLang(val) {
+        this.setState({ lng: val });
+    }
+
+    render() {
+        const lesson = {
+            "student": "Alyona",
+            "number": 31,
+            "slug": "ab"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <div className="pijne hidden">
+                    <button onClick={() => { this.setLang('def1') }} >`</button>
+                    <button onClick={() => { this.setLang('def2') }} >,</button>
+                </div>
+                <Mathilda words={words} lesnum={lesson.number} lang={this.state.lng} />
+            </div>
+        );
+    }
+}
+
+export default AB31;
