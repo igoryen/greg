d1d9d261d536a09674d64bfe210a7980ef8ddf2a
igoryen <igor.yentaltsev@gmail.com>
2025-05-27 22:02:34 -0400

    gp07: added

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ import GP01 from "./pages/gp/GP01";
 import GP02 from "./pages/gp/GP02";
 import GP03 from "./pages/gp/GP03";
 import GP06 from "./pages/gp/GP06";
+import GP07 from "./pages/gp/GP07";
 
 import Grm01 from "./pages/grm/Grm01";
 import Grm02 from "./pages/grm/Grm02";
@@ -247,6 +248,7 @@ export default function App() {
           <Route path="gp02" element={<GP02 />} />
           <Route path="gp03" element={<GP03 />} />
           <Route path="gp06" element={<GP06 />} />
+          <Route path="gp07" element={<GP07 />} />
 
           <Route path="grm01" element={<Grm01 />} />
           <Route path="grm02" element={<Grm02 />} />
---
+++ src/pages/gp/GP07.js
@@ -0,0 +1,40 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import Mathilda from "../../components/Mathilda";
+import words from "../../data/gp/gp07.json";
+
+class GP07 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: "def1"
+        }
+    }
+
+    setLang(val) {
+        this.setState({ lng: val });
+    }
+
+    render() {
+        const lesson = {
+            "student": "Galja",
+            "number": 7,
+            "slug": "gp"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <div className="pijne hidden">
+                    <button onClick={() => { this.setLang('def1') }} >`</button>
+                    <button onClick={() => { this.setLang('def2') }} >,</button>
+                </div>
+                <Mathilda words={words} lang={this.state.lng} lesnum={lesson.number} />
+            </div>
+        );
+    }
+}
+
+export default GP07;
