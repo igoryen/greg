0b9f6ba0874fcff9e155b434aba44422d6d85521
igoryen <igor.yentaltsev@gmail.com>
2025-05-29 12:47:40 -0400

    paused some

--- src/pages/Ang.js
+++ src/pages/Ang.js
@@ -9,7 +9,6 @@ class Ang extends React.Component {
                 <h1>Classes</h1>
                 <section className="tom">
                     <ol>
-                        <li><p><a href="/aa" rel="noopener noreferrer">aa</a></p></li>
                         <li><p><a href="/ab" rel="noopener noreferrer">ab</a></p></li>
                         <li><p><a href="/as" rel="noopener noreferrer">as</a></p></li>
                         <li><p><a href="/gp" rel="noopener noreferrer">gp</a></p></li>
@@ -17,7 +16,6 @@ class Ang extends React.Component {
                         <li><p><a href="/ik" rel="noopener noreferrer">ik</a></p></li>
                         <li><p><a href="/ip" rel="noopener noreferrer">ip</a></p></li>
                         <li><p><a href="/ml" rel="noopener noreferrer">ml</a></p></li>
-                        <li><p><a href="/ms" rel="noopener noreferrer">ms</a></p></li>
                         <li><p><a href="/os" rel="noopener noreferrer">os</a></p></li>
                         <li><p><a href="/ot" rel="noopener noreferrer">ot</a></p></li>
                         <li><p><a href="/sb" rel="noopener noreferrer">sb</a></p></li>
@@ -28,10 +26,12 @@ class Ang extends React.Component {
                 <h2>Paused</h2>
                 <section className="tom">
                     <ol>
+                        <li><p><a href="/aa" rel="noopener noreferrer">aa</a></p></li>
                         <li><p><a href="/ag" rel="noopener noreferrer">ag</a></p></li>
                         <li><p><a href="/gk" rel="noopener noreferrer">gk</a></p></li>
                         <li><p><a href="/im" rel="noopener noreferrer">im</a></p></li>
                         <li><p><a href="/is" rel="noopener noreferrer">is</a></p></li>
+                        <li><p><a href="/ms" rel="noopener noreferrer">ms</a></p></li>
                         <li><p><a href="/vk" rel="noopener noreferrer">vk</a></p></li>
                     </ol>
                 </section>
