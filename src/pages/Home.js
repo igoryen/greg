f4d3c8ea6afc835aeb4d7907f18e4eb29d904238
igoryen <igor.yentaltsev@gmail.com>
2025-06-11 00:47:23 -0400

    home: added no referrer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,10 +18,10 @@ const HomePage = () => {
                         <div>
                             Call me: <a href="tel:+16477036200" class=""> (647) 703-6200</a>
                         </div>
-                        <div>Instagram: <a href="https://www.instagram.com/anglofor" target="_blank">anglofor</a></div>
-                        <div>Telegram: <a href="https://t.me/anglofor" target="_blank">t.me/anglofor</a></div>
-                        <div>Facebook: <a href="https://www.facebook.com/profile.php?id=61569937586448" target="_blank">anglofor</a></div>
-                        <div>Email: <a href="mailto:anglofor2@gmail.com">anglofor2@gmail.com</a></div>
+                        <div>Instagram: <a href="https://www.instagram.com/anglofor" target="_blank" rel="noreferrer">anglofor</a></div>
+                        <div>Telegram: <a href="https://t.me/anglofor" target="_blank" rel="noreferrer">t.me/anglofor</a></div>
+                        <div>Facebook: <a href="https://www.facebook.com/profile.php?id=61569937586448" target="_blank" rel="noreferrer">anglofor</a></div>
+                        <div>Email: <a href="mailto:anglofor2@gmail.com" target="_blank" rel="noreferrer">anglofor2@gmail.com</a></div>
                     </div>
 
 
