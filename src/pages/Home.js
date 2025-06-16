800b257e36db0a7b55fe016c7c41680275f69a81
igoryen <igor.yentaltsev@gmail.com>
2025-06-16 11:06:40 -0400

    Home: string and color tweak

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,36 +13,33 @@ const HomePage = () => {
 
 
                 <div className="aaa">
-                    <div className="">
+                    <div className="kim">
 
-                        <h1>Anglofor</h1>
-                        <div className="matt">
-                            <div className="line1">English Lessons from Toronto</div>
-                            <div className="line2">For all levels – Life, Work & Everything Between</div>
-                        </div>
+                        <h1><i>Ang</i><i>lo</i><i>for</i></h1>
+                        <div className="jimmy">
+                            <div className="matt">
+                                <div className="line1">English Lessons from Toronto</div>
+                                <div className="line2">For all levels – Life, Work & Everything Between</div>
+                            </div>
 
-                        <div className="matt">
-                            <div className="line1">Clases de Inglés desde Toronto</div>
-                            <div className="line2">Para todos los niveles – Vida, Trabajo y Todo lo Demás</div>
-                        </div>
+                            <div className="matt">
+                                <div className="line1">Clases de Inglés desde Toronto</div>
+                                <div className="line2">Para todos los niveles – Vida, Trabajo y Todo lo Demás</div>
+                            </div>
 
-                        <div className="matt">
-                            <div className="line1">Английский из Торонто</div>
-                            <div className="line2">Для любого уровня — для жизни, работы и всего остального</div>
-                        </div>
-
-                        <div className="matt">
-                            <div className="line1">Lectiones Anglicae Toronti</div>
-                            <div className="line2">Omnibus gradibus – Vitae, Labori, et Ceteris Omnibus
-
+                            <div className="matt">
+                                <div className="line1">Английский из Торонто</div>
+                                <div className="line2">Для любого уровня — для жизни, работы и всего остального</div>
                             </div>
                         </div>
 
-                        <div>📞 🇨🇦: <a href="tel:+16477036200" className=""> (647) 703-6200</a></div>
-                        <div>Instagram: <a href="https://www.instagram.com/anglofor" target="_blank" rel="noreferrer">anglofor</a></div>
-                        <div>Telegram: <a href="https://t.me/anglofor" target="_blank" rel="noreferrer">t.me/anglofor</a></div>
-                        <div>Facebook: <a href="https://www.facebook.com/profile.php?id=61569937586448" target="_blank" rel="noreferrer">anglofor</a></div>
-                        <div>Email: <a href="mailto:anglofor2@gmail.com" target="_blank" rel="noreferrer">anglofor2@gmail.com</a></div>
+                        <div className="mike">
+                            <div>📞 🇨🇦: <a href="tel:+16477036200" className=""> (647) 703-6200</a></div>
+                            <div>Instagram: <a href="https://www.instagram.com/anglofor" target="_blank" rel="noreferrer">anglofor</a></div>
+                            <div>Telegram: <a href="https://t.me/anglofor" target="_blank" rel="noreferrer">t.me/anglofor</a></div>
+                            <div>Facebook: <a href="https://www.facebook.com/profile.php?id=61569937586448" target="_blank" rel="noreferrer">anglofor</a></div>
+                            <div>Email: <a href="mailto:anglofor2@gmail.com" target="_blank" rel="noreferrer">anglofor2@gmail.com</a></div>
+                        </div>
 
                     </div>
 
@@ -56,7 +53,7 @@ const HomePage = () => {
                 <a className="" href="/prenu" >🌲</a>
 
             </div>
-        </section>
+        </section >
     );
 }
 
