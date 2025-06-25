a4d6cb9fdd825b078cc027241a16e3fedfb6c31f
igoryen <igor.yentaltsev@gmail.com>
2025-06-25 10:04:25 -0400

    is.js moved to s

--- src/pages/s/is/IS01.js
+++ src/pages/s/is/IS01.js
@@ -2,7 +2,7 @@ import "../../../styles/fc.scss";
 import React from "react";
 import Leon from "../../../components/Leon";
 import Mathilda from "../../../components/Mathilda";
-import words from "../../../data/is/is01.json";
+import words from "../../../data/s/is/is01.json";
 
 class IS01 extends React.Component {
 
