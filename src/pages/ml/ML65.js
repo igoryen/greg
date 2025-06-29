7d0d79ee13f417ccb1e24d9e1e5d887f6b2825d2
igoryen <igor.yentaltsev@gmail.com>
2025-06-29 13:16:41 -0400

    ml65: added

---
+++ src/pages/ml/ML65.js
@@ -0,0 +1,25 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import Mathilda from "../../components/Mathilda";
+import words from "../../data/ml/ml65.json";
+
+class ML65 extends React.Component {
+
+    render() {
+        const lesson = {
+            "student": "Maria",
+            "number": 65,
+            "slug": "ml"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <Mathilda words={words} lesnum={lesson.number} />
+            </div>
+        );
+    }
+}
+
+export default ML65;
