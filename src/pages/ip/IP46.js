aaa6f94fa47da8465ab5a6dad3f11862f65872a6
igoryen <igor.yentaltsev@gmail.com>
2025-06-30 22:44:15 -0400

    ip46: added

---
+++ src/pages/ip/IP46.js
@@ -0,0 +1,25 @@
+import "../../styles/fc.scss";
+import React from "react";
+import Leon from "../../components/Leon";
+import words from "../../data/ip/ip46.json";
+import Mathilda from "../../components/Mathilda";
+
+class IP46 extends React.Component {
+
+    render() {
+        const lesson = {
+            "student": "Ihor",
+            "number": 46,
+            "slug": "ip"
+        };
+
+        return (
+            <div>
+                <Leon lesson={lesson} />
+                <Mathilda words={words} lesnum={lesson.number} />
+            </div>
+        );
+    }
+}
+
+export default IP46;
