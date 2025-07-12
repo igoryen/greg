6ecfde0cf490930fc58fdd9cc21357172abfb8a9
igoryen <igor.yentaltsev@gmail.com>
2025-07-12 01:04:22 -0400

    update Ang component links; replace 'ic' and 'os' with 'mg' and add 'yk' link

--- src/pages/Ang.js
+++ src/pages/Ang.js
@@ -12,16 +12,15 @@ class Ang extends React.Component {
                         <li><p><a href="/ab" rel="noopener noreferrer">ab</a></p></li>
                         <li><p><a href="/dk" rel="noopener noreferrer">dk</a></p></li>
                         <li><p><a href="/gp" rel="noopener noreferrer">gp</a></p></li>
-                        <li><p><a href="/ic" rel="noopener noreferrer">ic</a></p></li>
                         <li><p><a href="/ik" rel="noopener noreferrer">ik</a></p></li>
                         <li><p><a href="/ip" rel="noopener noreferrer">ip</a></p></li>
-                        <li><p><a href="/ml" rel="noopener noreferrer">ml</a></p></li>
-                        <li><p><a href="/os" rel="noopener noreferrer">os</a></p></li>
+                        <li><p><a href="/mg" rel="noopener noreferrer">mg</a></p></li>
+                        <li><p><a href="/student/ml" rel="noopener noreferrer">ml</a></p></li>
                         <li><p><a href="/ot" rel="noopener noreferrer">ot</a></p></li>
                         <li><p><a href="/sb" rel="noopener noreferrer">sb</a></p></li>
                         <li><p><a href="/ss" rel="noopener noreferrer">ss</a></p></li>
                         <li><p><a href="/st" rel="noopener noreferrer">st</a></p></li>
-                        <li><p><a href="/yk" rel="noopener noreferrer">yk</a></p></li>
+
                     </ol>
                 </section>
                 <h2>Paused</h2>
@@ -31,10 +30,13 @@ class Ang extends React.Component {
                         <li><p><a href="/ag" rel="noopener noreferrer">ag</a></p></li>
                         <li><p><a href="/as" rel="noopener noreferrer">as</a></p></li>
                         <li><p><a href="/gk" rel="noopener noreferrer">gk</a></p></li>
+                        <li><p><a href="/ic" rel="noopener noreferrer">ic</a></p></li>
                         <li><p><a href="/im" rel="noopener noreferrer">im</a></p></li>
                         <li><p><a href="/is" rel="noopener noreferrer">is</a></p></li>
                         <li><p><a href="/ms" rel="noopener noreferrer">ms</a></p></li>
+                        <li><p><a href="/os" rel="noopener noreferrer">os</a></p></li>
                         <li><p><a href="/vk" rel="noopener noreferrer">vk</a></p></li>
+                        <li><p><a href="/yk" rel="noopener noreferrer">yk</a></p></li>
                     </ol>
                 </section>
 
