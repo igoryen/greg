575576493a5901d44f365be0c0dbad7901f18bf0
igoryen <igor.yentaltsev@gmail.com>
2025-07-11 22:13:52 -0400

    remove ML68 component and its route; add lesson 68 to lessonsList.json

--- src/index.js
+++ src/index.js
@@ -183,7 +183,6 @@ import ML64 from "./pages/ml/ML64";
 import ML65 from "./pages/ml/ML65";
 import ML66 from "./pages/ml/ML66";
 import ML67 from "./pages/ml/ML67";
-import ML68 from "./pages/ml/ML68";
 
 import MS from "./pages/ms/MS";
 import MS06 from "./pages/ms/MS06";
@@ -471,7 +470,6 @@ export default function App() {
           <Route path="ml65" element={<ML65 />} />
           <Route path="ml66" element={<ML66 />} />
           <Route path="ml67" element={<ML67 />} />
-          <Route path="ml68" element={<ML68 />} />
 
           <Route path="ms" element={<MS />} />
           <Route path="ms06" element={<MS06 />} />
