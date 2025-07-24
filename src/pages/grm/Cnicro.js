fd78e6f93db3f9b9ca6df099b5f756f2054d3c36
igoryen <igor.yentaltsev@gmail.com>
2025-07-24 14:07:53 -0400

    cnicro: now 3 options, not 2

--- src/pages/grm/Cnicro.js
+++ src/pages/grm/Cnicro.js
@@ -2,14 +2,29 @@ import { React, useState } from "react";
 import "../../styles/grm/cnicro.scss";
 
 const Cnicro = (props) => {
-    const [showIPA, setShowIPA] = useState(true);
+
+    const [index, setIndex] = useState(0);
+
+    const here = ["hɪr", "here", "хир"];
+    const igh = ["aɪ", "I", "ай"];
+    const hesheit = ["hi ʃi ɪt", "he she it", "hи щи ɪт"];
+    const weyouthey = ["wi ju ðeɪ", "we you they", "ўи ю ðэй"];
+
+    const am = ["æm", "am", "æм"];
+    const ams = ["m", "'m", "м"];
+    const is = ["ɪz", "is", "ɪз"];
+    const iss = ["z s", "'s", "з с"];
+    const are = ["ɑr", "are", "ɑр"];
+    const ares = ["r", "'re", "р"];
+
+    const be = ["bi", "BE", "БИ"];
+    const not = ["nɑt", "not", "нɑт"];
+    const nots = ["nt", "n't", "нт"];
 
     return (
         <section className="cnicro">
             <div className="kimbo">
-                <button onClick={() => setShowIPA(!showIPA)}>
-                    {showIPA ? "ABC" : "IPA"}
-                </button>
+                <button onClick={() => setIndex((index + 1) % 3)}>Next</button>
             </div>
 
             <div className="jimbo">
@@ -17,128 +32,129 @@ const Cnicro = (props) => {
 
                     <h1>#1</h1>
                     <div className="snc positive">
-                        <div className="word pron _1ps">{showIPA ? `aɪ` : `I`}</div>
+                        <div className="word pron _1ps">{igh[index]}</div>
                         <div className="word verb _1ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `æm` : `am`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{am[index]}</div>
+                            <div className="baseForm">{be[index]}
+                            </div>
                         </div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc positive">
-                        <div className="word pron _1ps">{showIPA ? `aɪ` : `I`}</div>
+                        <div className="word pron _1ps">{igh[index]}</div>
                         <div className="word verb _1ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `m` : `'m`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{ams[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word">{here[index]}</div>
 
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _1ps">{showIPA ? `aɪ` : `I`}</div>
+                        <div className="word pron _1ps">{igh[index]}</div>
                         <div className="word verb _1ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `æm` : `am`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{am[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _1ps">{showIPA ? `aɪ` : `I`}</div>
+                        <div className="word pron _1ps">{igh[index]}</div>
                         <div className="word verb _1ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `m` : `m`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{ams[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
-
 
                     <div className="snc question">
                         <div className="word verb _1ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `æm` : `am`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{am[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word pron _1ps">{showIPA ? `aɪ` : `I`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word pron _1ps">{igh[index]}</div>
+                        <div className="word">{here[index]}</div>
                         <div className="punctuation">?</div>
 
                     </div>
+
                 </div>
 
                 <div className="sncBlock">
                     <h1>#2</h1>
 
                     <div className="snc positive">
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɪz` : `is`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{is[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word"><div className="word">{showIPA ? `hɪr` : `here`}</div></div>
+                        <div className="word">
+                            <div className="word">{here[index]}</div>
+                        </div>
                     </div>
 
                     <div className="snc positive">
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `z` : ``}</div>
-                            <div className="avatar">{showIPA ? `s` : `'s`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{iss[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɪz` : `is`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{is[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `z` : ``}</div>
-                            <div className="avatar">{showIPA ? `s` : `'s`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{iss[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɪz` : `is`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{is[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nt` : `n't`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{nots[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc question">
                         <div className="word verb _3ps be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɪz` : `is`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{is[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word pron _3ps">{showIPA ? `hi ʃi ɪt` : `he she it`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word pron _3ps">{hesheit[index]}</div>
+                        <div className="word">{here[index]}</div>
                         <div className="punctuation">?</div>
                     </div>
                 </div>
@@ -147,66 +163,66 @@ const Cnicro = (props) => {
 
                     <h1>#3</h1>
                     <div className="snc positive">
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɑr` : `are`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{are[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc positive">
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `r` : `'re`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{ares[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɑr` : `are`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{are[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `r` : `'re`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{ares[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nɑt` : `not`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{not[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc negative">
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɑr` : `are`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{are[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word adv not">{showIPA ? `nt` : `n't`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word adv not">{nots[index]}</div>
+                        <div className="word">{here[index]}</div>
                     </div>
 
                     <div className="snc question">
                         <div className="word verb _pl be">
                             <div className="emoji">🐝</div>
-                            <div className="avatar">{showIPA ? `ɑr` : `are`}</div>
-                            <div className="baseForm">{showIPA ? `bi` : `BE`}</div>
+                            <div className="avatar">{are[index]}</div>
+                            <div className="baseForm">{be[index]}</div>
                         </div>
-                        <div className="word pron _pl">{showIPA ? `wi ju ðeɪ` : `we you they`}</div>
-                        <div className="word">{showIPA ? `hɪr` : `here`}</div>
+                        <div className="word pron _pl">{weyouthey[index]}</div>
+                        <div className="word">{here[index]}</div>
                         <div className="punctuation">?</div>
                     </div>
                 </div>
