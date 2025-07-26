99489e146a3bca169ddb12ae9c0417d09de58e67
igoryen <igor.yentaltsev@gmail.com>
2025-07-26 00:06:19 -0400

    prenu: obj pronouns added

--- src/pages/grm/Prenu.js
+++ src/pages/grm/Prenu.js
@@ -2,32 +2,34 @@ import "../../styles/grm/prenu.scss";
 
 const Prenu = (props) => {
     return (
-        <section className="prenu">
+        <section id="prenu">
+
             <div className="container">
+
                 <div className="floor p3 skin">
                     <div className="skin room">
                         <div className="skin box">
                             <div className="skin pron">he</div>
-                            <div className="skin poss">his</div>
+                            <div className="skin poss"><b className="orig">h</b>is</div>
                         </div>
 
                         <div className="skin box">
                             <div className="skin pron">she</div>
-                            <div className="skin poss">he<i>r</i></div>
+                            <div className="skin poss">h<b className="orig">e</b><i>r</i></div>
                         </div>
                         <div className="skin box">
                             <div className="skin pron">it</div>
-                            <div className="skin poss">it<i>s</i></div>
+                            <div className="skin poss"><b className="orig">it</b><i>s</i></div>
                         </div>
                     </div>
                     <div className="skin room box">
                         <div className="skin pron">they</div>
-                        <div className="skin poss">thei<i>r</i></div>
+                        <div className="skin poss"><b className="orig">the</b>i<i>r</i></div>
                     </div>
                 </div>
                 <div className="floor p2 skin">
                     <div className="room pron">you</div>
-                    <div className="room poss">you<i>r</i></div>
+                    <div className="room poss"><b className="orig">you</b><i>r</i></div>
                 </div>
                 <div className="floor p1 skin">
                     <div className="skin room box">
@@ -50,6 +52,69 @@ const Prenu = (props) => {
                 <p>It has <i>its</i> shoes.</p>
                 <p>We have <i>our</i> shoes.</p>
                 <p>They have <i>their</i> shoes.</p>
+            </div>
+
+
+            <div className="container">
+                <div className="floor p3 skin">
+                    <div className="skin room">
+                        <div className="skin box">
+                            <div className="skin pron">he</div>
+                            <div className="skin obj"><b className="orig">h</b>im</div>
+                        </div>
+
+                        <div className="skin box">
+                            <div className="skin pron">she</div>
+                            <div className="skin obj">h<b className="orig">e</b><i>r</i></div>
+                        </div>
+                        <div className="skin box">
+                            <div className="skin pron">it</div>
+                            <div className="skin obj"><b className="orig">it</b></div>
+                        </div>
+                    </div>
+                    <div className="skin room box">
+                        <div className="skin pron">they</div>
+                        <div className="skin obj"><b className="orig">the</b>m</div>
+                    </div>
+                </div>
+                <div className="floor p2 skin">
+                    <div className="room pron">you</div>
+                    <div className="room obj"><b className="orig">you</b></div>
+                </div>
+                <div className="floor p1 skin">
+                    <div className="skin room box">
+                        <div className="skin pron">I</div>
+                        <div className="skin obj">me</div>
+                    </div>
+                    <div className="skin room box">
+                        <div className="skin pron">we</div>
+                        <div className="skin obj">us</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="henry">
+                <div className="mike">
+                    <p>👁️👁️👓👀🤓</p>
+                    <p>I see <i>him</i>.</p>
+                    <p>You see <i>them</i>.</p>
+                    <p>He sees <i>me</i>.</p>
+                    <p>She sees <i>us</i>.</p>
+                    <p>It sees <i>her</i>.</p>
+                    <p>We see <i>you</i>.</p>
+                    <p>They see <i>it</i>.</p>
+                </div>
+
+                <div className="mike">
+                    <p>🗣️🔊🎤</p>
+                    <p>I talk with <i>him</i>.</p>
+                    <p>You talk with  <i>them</i>.</p>
+                    <p>He talks with  <i>me</i>.</p>
+                    <p>She talks with  <i>us</i>.</p>
+                    <p>It talks with  <i>her</i>.</p>
+                    <p>We talk with  <i>you</i>.</p>
+                    <p>They talk with  <i>it</i>.</p>
+                </div>
             </div>
 
         </section>
