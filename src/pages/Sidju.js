ad6501ddd4543abb2c11c86b545f6c16a67723b5
igoryen <igor.yentaltsev@gmail.com>
2025-08-06 12:11:53 -0400

    Sidju: updated

--- src/pages/Sidju.js
+++ src/pages/Sidju.js
@@ -6,12 +6,15 @@ const Sidju = () => {
 
     const pages = [
         { label: "Alfabeta", path: "/alfabeta", desc: "the names of the letters" },
+        { label: "Clani", path: "/clani", desc: "vowels: long, short" },
         { label: "Cnicro", path: "/cnicro", desc: "\"be\" in present simple" },
+        { label: "Gadri", path: "/gadri", desc: "articles: ə/ən, ðə/ði" },
         { label: "Jimte", path: "/jimte", desc: "random word wrapper" },
         { label: "Kooi", path: "/kooi", desc: "imperative" },
         { label: "Prenu", path: "/prenu", desc: "possessive pronouns" },
         { label: "Rimni", path: "/rimni", desc: "IPA symbols" },
         { label: "Ruo", path: "/ruo", desc: "IPA to Cyrillic converter" },
+        { label: "Smuni", path: "/smuni", desc: "Interlinear texts" },
         { label: "Uenba", path: "/uenba", desc: "sound drilling" },
         { label: "Voksa", path: "/voksa", desc: "Insta" },
         { label: "Zunsna", path: "/zunsna", desc: "phoneme <s>" },
