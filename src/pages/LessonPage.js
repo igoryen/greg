e24cab6285d817a297375fbeb656234b8b6dc630
igoryen <igor.yentaltsev@gmail.com>
2025-08-24 22:35:13 -0400

    Revert "link preview: added for lesson page: using Helmet"
    This reverts commit 59b0aef0a24a334b4067a1ffb7d8aacaeefcdd52.

--- src/pages/LessonPage.js
+++ src/pages/LessonPage.js
@@ -2,7 +2,6 @@ import { useParams, Link } from "react-router-dom";
 // import Leon from "../components/Leon";
 import Mathilda from "../components/Mathilda";
 import { useEffect, useState } from "react";
-import { Helmet } from "react-helmet-async";
 
 function LessonPage() {
     const { slug, number } = useParams();
@@ -34,38 +33,13 @@ function LessonPage() {
     // Capitalize the period ("Lesson" or "Week")
     const periodLabel = lessonData.period.charAt(0).toUpperCase() + lessonData.period.slice(1);
 
-    // Show only year if period === "week"
+    // 🔽 Show only year if period === "week"
     const displayDate = lessonData.period === "week" && lessonData.date
         ? new Date(lessonData.date).getFullYear()
         : lessonData.date;
 
-    // Meta title & description for previews
-    const metaTitle = `${lessonData.student} - ${periodLabel} ${lessonData.number}`;
-    const metaDescription = `${lessonData.student}'s ${periodLabel.toLowerCase()} ${lessonData.number} on ${displayDate}.`;
-
-    // Default preview image (place this inside your public/ folder)
-    const previewImage = `${window.location.origin}/preview.jpg`;
-
     return (
         <div className="pelli">
-            <Helmet>
-                <title>{metaTitle}</title>
-                <meta name="description" content={metaDescription} />
-
-                {/* Open Graph tags for social previews */}
-                <meta property="og:title" content={metaTitle} />
-                <meta property="og:description" content={metaDescription} />
-                <meta property="og:type" content="article" />
-                <meta property="og:url" content={window.location.href} />
-                <meta property="og:image" content={previewImage} />
-
-                {/* Twitter card */}
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={metaTitle} />
-                <meta name="twitter:description" content={metaDescription} />
-                <meta name="twitter:image" content={Image} />
-            </Helmet>
-
             <div className="linus">
                 <Link to={`/student/${slug}`}>{lessonData.student}</Link>
             </div>
