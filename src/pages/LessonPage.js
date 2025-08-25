c0599afd9fb33047991eafb0a9ab226949d40f43
igoryen <igor.yentaltsev@gmail.com>
2025-08-24 23:35:46 -0400

    Helmet: added

--- src/pages/LessonPage.js
+++ src/pages/LessonPage.js
@@ -2,6 +2,7 @@ import { useParams, Link } from "react-router-dom";
 // import Leon from "../components/Leon";
 import Mathilda from "../components/Mathilda";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 
 function LessonPage() {
     const { slug, number } = useParams();
@@ -33,6 +34,9 @@ function LessonPage() {
     // Capitalize the period ("Lesson" or "Week")
     const periodLabel = lessonData.period.charAt(0).toUpperCase() + lessonData.period.slice(1);
 
+    // Meta title & description for previews
+    const metaTitle = `${lessonData.student} - ${periodLabel} ${lessonData.number}`;
+
     // 🔽 Show only year if period === "week"
     const displayDate = lessonData.period === "week" && lessonData.date
         ? new Date(lessonData.date).getFullYear()
@@ -40,6 +44,9 @@ function LessonPage() {
 
     return (
         <div className="pelli">
+            <Helmet>
+                <title>{metaTitle}</title>
+            </Helmet>
             <div className="linus">
                 <Link to={`/student/${slug}`}>{lessonData.student}</Link>
             </div>
