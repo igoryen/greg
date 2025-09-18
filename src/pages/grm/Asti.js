76a19f53942ed7cd1a37afac6f9617f34ab9241b
igoryen <igor.yentaltsev@gmail.com>
2025-09-17 21:39:32 -0400

    Asti: updated

--- src/pages/grm/Asti.js
+++ src/pages/grm/Asti.js
@@ -5,64 +5,203 @@ const Asti = (prop) => {
         <section id="asti">
 
             <h1>The existential <b className="there">there</b></h1>
-            <div>
-                <div className="line">
-                    <b className="group"><b className="there">There</b> <b className="be">is</b></b>
-                    <b className="sing">an object</b>
-                    there.
-                </div>
-                <div className="line">
-                    <b className="group">
-                        <b className="there">There</b> <b className="be">is</b></b> <b className="not">not</b>
-                    <b className="sing">an object</b> there.
-                </div>
-                <div className="line">
-                    <b className="group">
-                        <b className="be">Is</b> <b className="there">There</b>
-                    </b>
-                    <b className="sing">an object</b> there<b className="que">?</b>
-                </div>
-                <div className="line">Yes,
-                    <b className="group"><b className="there">There</b> <b className="be">is</b></b>.
-                </div>
-                <div className="line">No,
-                    <b className="group">
-                        <b className="there">There</b> <b className="be">is</b>
-                    </b>
-                    <b className="not">not</b>.
+            <div className="groups">
+                <div className="group">
+
+                    <div className="line">
+                        <b className="group"><b className="there">There</b>
+                            <b className="be">is</b></b>
+                        <b className="sing indefinite">a car</b>
+                        there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group"><b className="there">There</b>
+                            <b className="be">is</b>
+                        </b>
+                        <b className="some">some</b>
+                        <b className="sing indefinite">car</b>
+                        there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b>
+                            <b className="be">is</b></b>
+                        <b className="sing definite">the car</b>
+                        there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b>
+                            <b className="be">is</b>
+                        </b>
+                        <b className="no">NO</b>
+                        <b className="sing indefinite">car</b> there.
+                    </div>
+
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">is</b>
+                            <b className="not">not</b>
+                        </b>
+
+                        <b className="sing indefinite">a car</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">is</b>
+                            <b className="not">not</b>
+                        </b>
+                        <b className="any">any</b>
+                        <b className="sing indefinite">car</b> there.
+                    </div>
+
+
+
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Is</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="sing indefinite">a car</b>
+                        there
+                        <b className="que">?</b>
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Is</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="any">any</b>
+                        <b className="sing indefinite">car</b>
+                        there
+                        <b className="que">?</b>
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Is</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="sing definite">the car</b>
+                        there
+                        <b className="que">?</b>
+                    </div>
+
+                    <div className="line">Yes,
+                        <b className="group"><b className="there">There</b> <b className="be">is</b></b>.
+                    </div>
+                    <div className="line">No,
+                        <b className="group">
+                            <b className="there">There</b>
+                            <b className="be">is</b>
+                            <b className="not">not</b>.
+                        </b>
+
+                    </div>
                 </div>
 
-                <div className="line">
-                    <b className="group">
-                        <b className="there">There</b> <b className="be">are</b>
-                    </b>
-                    10 <b className="plur">objects</b> there.
-                </div>
-                <div className="line">
-                    <b className="group"><b className="there">There</b> <b className="be">are</b> </b><b className="some">some</b>
-                    <b className="plur">objects</b> there.
-                </div>
+                <div className="group">
 
-                <div className="line">
-                    <b className="group">
-                        <b className="there">There</b> <b className="be">are</b></b>
-                    <b className="not">not</b> <b className="any">any</b>
-                    <b className="plur">objects</b> there.
-                </div>
-                <div className="line">
-                    <b className="group">
-                        <b className="be">Are</b> <b className="there">There</b>
-                    </b>
-                    <b className="any">any</b>
-                    <b className="plur">objects</b> there<b className="que">?</b></div>
-                <div className="line">Yes,
-                    <b className="group"><b className="there">There</b> <b className="be">are</b>
-                    </b>.
-                </div>
-                <div className="line">No,
-                    <b className="group"><b className="there">There</b> <b className="be">are</b>
-                    </b>
-                    <b className="not">not</b>.
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b>
+                        </b>
+                        10 <b className="plur indefinite">cars</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b>
+                        </b>
+                        <b className="some">some</b>
+                        <b className="plur indefinite">cars</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b>
+                        </b>
+                        <b className="plur definite"> the cars</b> there.
+                    </div>
+
+
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b></b>
+                        <b className="no">NO</b>
+                        <b className="plur indefinite">cars</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b>
+                            <b className="not">not</b>
+                        </b>
+
+                        <b className="plur indefinite">cars</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="there">There</b> <b className="be">are</b>
+                            <b className="not">not</b>
+                        </b>
+                        <b className="any">any</b>
+                        <b className="plur indefinite">cars</b> there.
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Are</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="plur indefinite">cars</b> there
+                        <b className="que">?</b>
+                    </div>
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Are</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="any">any</b>
+                        <b className="plur indefinite">cars</b> there
+                        <b className="que">?</b>
+                    </div>
+
+
+                    <div className="line">
+                        <b className="group">
+                            <b className="be">Are</b>
+                            <b className="there">There</b>
+                        </b>
+                        <b className="plur definite">the cars</b> there
+                        <b className="que">?</b>
+                    </div>
+
+
+
+
+                    <div className="line">Yes,
+                        <b className="group"><b className="there">There</b> <b className="be">are</b>
+                        </b>.
+                    </div>
+                    <div className="line">No,
+                        <b className="group">
+                            <b className="there">There</b>
+                            <b className="be">are</b>
+                            <b className="not">not</b>
+                        </b>
+                        .
+                    </div>
                 </div>
 
             </div>
