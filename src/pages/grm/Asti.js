c1f0adb332cf62fa3251adc74770c50590c0140f
igoryen <igor.yentaltsev@gmail.com>
2025-10-15 00:02:56 -0400

    asti: pruned, weeded

--- src/pages/grm/Asti.js
+++ src/pages/grm/Asti.js
@@ -16,23 +16,6 @@ const Asti = (prop) => {
                     </div>
 
                     <div className="line">
-                        <b className="group"><b className="there">There</b>
-                            <b className="be">is</b>
-                        </b>
-                        <b className="some">some</b>
-                        <b className="sing indefinite">car</b>
-                        there.
-                    </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="there">There</b>
-                            <b className="be">is</b></b>
-                        <b className="sing definite">the car</b>
-                        there.
-                    </div>
-
-                    <div className="line">
                         <b className="group">
                             <b className="there">There</b>
                             <b className="be">is</b>
@@ -41,27 +24,13 @@ const Asti = (prop) => {
                         <b className="sing indefinite">car</b> there.
                     </div>
 
-
                     <div className="line">
                         <b className="group">
                             <b className="there">There</b> <b className="be">is</b>
                             <b className="not">not</b>
                         </b>
-
                         <b className="sing indefinite">a car</b> there.
                     </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="there">There</b> <b className="be">is</b>
-                            <b className="not">not</b>
-                        </b>
-                        <b className="any">any</b>
-                        <b className="sing indefinite">car</b> there.
-                    </div>
-
-
-
 
                     <div className="line">
                         <b className="group">
@@ -69,27 +38,6 @@ const Asti = (prop) => {
                             <b className="there">There</b>
                         </b>
                         <b className="sing indefinite">a car</b>
-                        there
-                        <b className="que">?</b>
-                    </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="be">Is</b>
-                            <b className="there">There</b>
-                        </b>
-                        <b className="any">any</b>
-                        <b className="sing indefinite">car</b>
-                        there
-                        <b className="que">?</b>
-                    </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="be">Is</b>
-                            <b className="there">There</b>
-                        </b>
-                        <b className="sing definite">the car</b>
                         there
                         <b className="que">?</b>
                     </div>
@@ -103,7 +51,6 @@ const Asti = (prop) => {
                             <b className="be">is</b>
                             <b className="not">not</b>.
                         </b>
-
                     </div>
                 </div>
 
@@ -126,15 +73,6 @@ const Asti = (prop) => {
 
                     <div className="line">
                         <b className="group">
-                            <b className="there">There</b> <b className="be">are</b>
-                        </b>
-                        <b className="plur definite"> the cars</b> there.
-                    </div>
-
-
-
-                    <div className="line">
-                        <b className="group">
                             <b className="there">There</b> <b className="be">are</b></b>
                         <b className="no">NO</b>
                         <b className="plur indefinite">cars</b> there.
@@ -145,26 +83,8 @@ const Asti = (prop) => {
                             <b className="there">There</b> <b className="be">are</b>
                             <b className="not">not</b>
                         </b>
-
-                        <b className="plur indefinite">cars</b> there.
-                    </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="there">There</b> <b className="be">are</b>
-                            <b className="not">not</b>
-                        </b>
                         <b className="any">any</b>
                         <b className="plur indefinite">cars</b> there.
-                    </div>
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="be">Are</b>
-                            <b className="there">There</b>
-                        </b>
-                        <b className="plur indefinite">cars</b> there
-                        <b className="que">?</b>
                     </div>
 
                     <div className="line">
@@ -176,19 +96,6 @@ const Asti = (prop) => {
                         <b className="plur indefinite">cars</b> there
                         <b className="que">?</b>
                     </div>
-
-
-                    <div className="line">
-                        <b className="group">
-                            <b className="be">Are</b>
-                            <b className="there">There</b>
-                        </b>
-                        <b className="plur definite">the cars</b> there
-                        <b className="que">?</b>
-                    </div>
-
-
-
 
                     <div className="line">Yes,
                         <b className="group"><b className="there">There</b> <b className="be">are</b>
