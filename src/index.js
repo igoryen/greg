26401559d318ff1f21332b926c7b40a9c5757f82
igoryen <igor.yentaltsev@gmail.com>
2025-11-16 08:58:08 -0500

    ip29: added

--- src/index.js
+++ src/index.js
@@ -117,7 +117,6 @@ import IM10 from "./pages/im/IM10";
 import IM12 from "./pages/im/IM12";
 
 import IP from "./pages/ip/IP";
-import IP29 from "./pages/ip/IP29";
 import IP30 from "./pages/ip/IP30";
 import IP31 from "./pages/ip/IP31";
 import IP33 from "./pages/ip/IP33";
@@ -437,7 +436,6 @@ export default function App() {
           <Route path="im12" element={<IM12 />} />
 
           <Route path="ip" element={<IP />} />
-          <Route path="ip29" element={<IP29 />} />
           <Route path="ip30" element={<IP30 />} />
           <Route path="ip31" element={<IP31 />} />
           <Route path="ip33" element={<IP33 />} />
