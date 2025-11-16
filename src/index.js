1a74f6752b9fe417399a631c32e58add9eb627a0
igoryen <igor.yentaltsev@gmail.com>
2025-11-16 12:36:21 -0500

    ik16: redone

--- src/index.js
+++ src/index.js
@@ -97,7 +97,6 @@ import Grm01 from "./pages/grm/Grm01";
 import Grm02 from "./pages/grm/Grm02";
 
 import IK from "./pages/ik/IK";
-import IK16 from "./pages/ik/IK16";
 import IK17 from "./pages/ik/IK17";
 import IK18 from "./pages/ik/IK18";
 import IK19 from "./pages/ik/IK19";
@@ -396,7 +395,6 @@ export default function App() {
           <Route path="grm02" element={<Grm02 />} />
 
           <Route path="ik" element={<IK />} />
-          <Route path="ik16" element={<IK16 />} />
           <Route path="ik17" element={<IK17 />} />
           <Route path="ik18" element={<IK18 />} />
           <Route path="ik19" element={<IK19 />} />
