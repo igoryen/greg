43fd3355b6fd71af6bb3c5f73c7fba5ecbfb1fa8
Igor Entaltsev <igor.yentaltsev@gmail.com>
2025-12-21 18:40:25 -0500

    Helmet: added default values, rearranged a bit

--- src/components/SEOHelmet.js
+++ src/components/SEOHelmet.js
@@ -2,17 +2,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-const SEOHelmet = ({ title, description, url, image }) => {
+const SEOHelmet = ({
+    title = "Anglofor",
+    description = "Professional English tutoring.",
+    url = "https://anglofor.com",
+    image = "https://anglofor.com/images/preview-image-1200x630.jpeg",
+}) => {
     return (
         <Helmet>
             <title>{title}</title>
             <meta name="description" content={description} />
 
             {/* Open Graph (Facebook, LinkedIn, WhatsApp, etc.) */}
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
             <meta property="og:url" content={url} />
             <meta property="og:type" content="website" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
@@ -26,7 +31,7 @@ const SEOHelmet = ({ title, description, url, image }) => {
             <meta name="twitter:site" content="@anglofor" />
 
             {/* Additional important tags */}
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
             <link rel="canonical" href={url} />
         </Helmet>
     );
