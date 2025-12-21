53c2e899587bd08c124ffc3f5db46f6c91ee1443
Igor Entaltsev <igor.yentaltsev@gmail.com>
2025-12-21 18:56:40 -0500

    Linsi: Linkglimpse added

--- src/pages/Linsi.js
+++ src/pages/Linsi.js
@@ -20,6 +20,7 @@ const Linsi = (prop) => {
         { name: "Lojban dictionary", url: "https://la-lojban.github.io/sutysisku/lojban/index.html" },
         { name: "Comparative sentence (wiki)", url: "https://en.wikipedia.org/wiki/Comparative_sentence" },
         { name: "Shaw-Spell", url: "https://joro.io/shaw-spell/" },
+        { name: "Linkglimpse", url: "https://www.linkglimpse.com/" }
 
     ];
 
